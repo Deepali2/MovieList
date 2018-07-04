9a3ca98d2ef87f4c63bf3c6695a1478a636a6eca
Deepali Garg <hdgarg@gmail.com>
2018-07-04 13:05:15 -0700

    adds example data as a seperate file

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Movies from "./Movies.js";
+import {movies} from "./example_data.js";
 
 class MovieList extends React.Component {
   render() {
     return (
-      <h1>Movie List</h1>
+      <div>
+        <h1>Movie List</h1>
+        <Movies movies = {movies}/>
+      </div>      
     );
   }
  
 }
+
 export default MovieList;
