22b2d3d0fd1b6f188058d4d5471ffcfcbd15e215
Deepali Garg <hdgarg@gmail.com>
2018-07-04 14:25:17 -0700

    connects css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import MovieList from "./MovieList.js";
+import "./styles/style.css";
 
 ReactDOM.render(<MovieList />, document.getElementById("index"));
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
+            loader: "style-loader"
+          },
+          {
             loader: "css-loader",
             options: {
               modules: true,
