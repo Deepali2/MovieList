f870e9eb928aa1466896d387bba984dedf78651d
Deepali Garg <hdgarg@gmail.com>
2018-07-04 14:05:02 -0700

    renders the movie titles

--- src/MovieEntry.js
+++ src/MovieEntry.js
@@ -1,6 +1,19 @@
 import React from "react";
-class MovieEntry extends React.Component {
+import PropTypes from 'prop-types';
 
+
+class MovieEntry extends React.Component {
+  render() {
+    return (
+      <h3>{this.props.movie.title}</h3>
+    );
+  }
 }
+
+MovieEntry.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string
+  }).isRequired
+};
 
 export default MovieEntry;
--- src/Movies.js
+++ src/Movies.js
@@ -1,7 +1,17 @@
 import React from "react";
 import MovieEntry from "./MovieEntry.js";
+import { movies } from "./example_data.js";
 
 class Movies extends React.Component {
+  render() {
+    return (
+      <div>
+        {movies.map(movie => <MovieEntry movie={movie}/>)}                
+      </div>
+    );
+  }
 
 }
+
+
 export default Movies;
