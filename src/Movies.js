1ef69ac89090199d1624249618569104279d6cf9
Deepali Garg <hdgarg@gmail.com>
2018-07-04 19:21:20 -0700

    completes level 1 Search button working

--- src/Movies.js
+++ src/Movies.js
@@ -1,22 +1,17 @@
 import React from "react";
 import MovieEntry from "./MovieEntry.js";
-import { movies } from "./example_data.js";
+// import { movies } from "./example_data.js";
 import style from "./styles/style.css"
 
 
-class Movies extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className={style.BlankSpace}>
-        </div>
-          <div className={style.movies}>
-            {movies.map((movie, index) => <MovieEntry movie={movie} key={`${movie}${index}`}/>)}
-        </div>  
-      </div>    
-    );
-  }
-
+var Movies = (props) => {  
+  return (
+    <div>      
+        <div className={style.movies}>
+          {props.movies.map((movie, index) => <MovieEntry movie={movie} key={`${movie}${index}`}/>)}
+      </div>  
+    </div>    
+  );
 }
 
 
