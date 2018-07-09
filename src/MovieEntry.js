614c0a3b2e2f64685d3f50ff9bfe0b7114ee84ea
Deepali Garg <hdgarg@gmail.com>
2018-07-08 21:32:05 -0700

    toggles movie details

--- src/MovieEntry.js
+++ src/MovieEntry.js
@@ -5,19 +5,62 @@ import { WATCHED_TEXT, NOT_WATCHED_TEXT } from "./MovieConstants.js";
 
 const watchedText = (watched) => watched ? WATCHED_TEXT : NOT_WATCHED_TEXT; 
 
-var MovieEntry = (props) => {  
- 
+class MovieEntry extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state={
+      showDetails: false,
+    }
+  }
+
+
+  render() {
+    return (
+      <div className={style.MovieEntry}>        
+        <div onClick={() => this.setState(prevState=>({
+          showDetails: !prevState.showDetails
+        })) }> 
+          {this.props.movie.title}   
+        </div>
+        <button className={this.props.movie.watchStatus ? style.watchStatusBtnGreen : style.watchStatusBtn}
+          onClick={this.props.watchedClickHandler(this.props.index)}
+        >
+          {watchedText(this.props.movie.watchStatus)}
+        </button>
+        
+        {this.state.showDetails && (
+        <div>
+          year: 1995
+          Runtime: 107 min
+          Metascore: 46
+          imdbRating: 6.2
+          Watched:
+        </div>)
+      }
+      </div>
+      
+    ); 
+
+  }
+   
+}
+
+/*
+var MovieEntry = (props) => {
+
   return (
       <div className={style.MovieEntry}>
         {props.movie.title}
-      <button className={props.movie.watchStatus ? style.watchStatusBtnGreen : style.watchStatusBtn}         
+      <button className={props.movie.watchStatus ? style.watchStatusBtnGreen : style.watchStatusBtn}
         onClick={props.watchedClickHandler(props.index)}
       >
         {watchedText(props.movie.watchStatus)}
       </button>
       </div>
-  );  
+  );
 }
+
+*/
 
 MovieEntry.propTypes = {
   movie: PropTypes.shape({
